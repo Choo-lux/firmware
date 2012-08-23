f42e85f49c433e666600d5707fd0dbea380c9a33
Cody Cooper <cody@codycooper.co.nz>
2012-08-23 21:11:50 +1200

    Fixes bug with internet checker

--- wifimesh/files/sbin/wifimesh/check.sh
+++ wifimesh/files/sbin/wifimesh/check.sh
@@ -8,35 +8,11 @@
 echo "WiFi Mesh Connection Checker"
 echo "----------------------------------------------------------------"
 
-# Test Global Internet Connectivity (traceroutes the DNS Root Servers)
-#test_root=( "198.41.0.4" "192.228.79.201" "192.33.4.12" "128.8.10.90" "192.203.230.10" "192.5.5.241" "192.112.36.4" "128.63.2.53" "192.36.148.17" "192.58.128.30" "193.0.14.129" "199.7.83.42" "202.12.27.33" )
-#test_root_num=${#test_root[@]}
-#rand=${test_root[$((RANDOM%test_root_num))]}
-rand="202.12.27.33"
-
-# If we can traceroute, we are connected
-if [ "$(traceroute -n -w 1 -q 1 -m 20 $rand)" ]; then
+# Test Global Internet Connectivity (connects to an HTTP server)
+if [ "$(curl http://www.google.com/)" ]; then
 	connected=1
 else
-	# Test Internet Connectivity (curl GETs a popular webpage)
-	#test_popular=(
-		#"http://www.google.com/"
-		#"http://www.youtube.com/"
-		#"http://search.yahoo.com/"
-		#"http://www.wikipedia.org/"
-	#)
-	#test_popular_num=${#test_popular[@]}
-	#site=${test_popular[$((RANDOM%test_popular_num))]}
-	site="http://www.google.com/"
-	
-	# If we can get a html page back, we are connected
-	if [ "$(curl -A 'WMF/v${fw_ver} (http://www.wifi-mesh.com/)' -s $site | grep -i -c '<html')" -eq "1" ]; then
-		connected=1
-	
-	# Or else we must not be
-	else
-		connected=0
-	fi
+	connected=0
 fi
 
 # If we are connected
