5dc225395f28d7365697996cb69fd6e1daef438d
Cody Cooper <cody@codycooper.co.nz>
2012-08-29 02:47:26 +1200

    Fixes bug with bug fixing DNS issue
    ;)

--- wifimesh/files/sbin/wifimesh/startup.sh
+++ wifimesh/files/sbin/wifimesh/startup.sh
@@ -142,7 +142,7 @@ elif [ -z $dns2 ]; then
 	dns2=1
 fi
 done
-curl -A "WMF/v${fw_ver} (http://www.wifi-mesh.com/)" -k -o /etc/chilli/defaults "https://www.wifi-mesh.com/dashboard/checkin-wm.php?ip=${ip}&mac_lan=${mac_lan}&mac_wan=${mac_wan}&mac_wlan=${mac_wlan}&action=coova-config&$(cat /tmp/dns.tmp)"
+curl -A "WMF/v${fw_ver} (http://www.wifi-mesh.com/)" -k -o /etc/chilli/defaults "https://www.wifi-mesh.com/dashboard/checkin-wm.php?ip=${ip}&mac_lan=${mac_lan}&mac_wan=${mac_wan}&mac_wlan=${mac_wlan}&action=coova-config&$(sed ':a;N;$!ba;s/\n//g' /tmp/dns.tmp)"
 
 log_message "boot: getting coova logo"
 curl -A "WMF/v${fw_ver} (http://www.wifi-mesh.com/)" -k -o /etc/chilli/www/coova.jpg "https://www.wifi-mesh.com/dashboard/checkin-wm.php?ip=${ip}&mac_lan=${mac_lan}&mac_wan=${mac_wan}&mac_wlan=${mac_wlan}&action=coova-logo"
