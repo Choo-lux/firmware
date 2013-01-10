08299ee848b5b322ced16959c40c2c91c9fd7d5a
Cody Cooper <cody@codycooper.co.nz>
2013-01-11 11:57:01 +1300

    Moves the coova.html file into the actual directory if necessary

--- wifimesh/files/sbin/wifimesh/startup.sh
+++ wifimesh/files/sbin/wifimesh/startup.sh
@@ -162,6 +162,11 @@ HS_RAD_PROTO='chap'" > /etc/chilli/defaults
 /etc/init.d/chilli enable
 /etc/init.d/chilli start
 
+# Move the firmware default coova.html file into the actual directory, if necessary
+if [ -e "/sbin/wifimesh/coova.html" ]; then
+	mv /sbin/wifimesh/coova.html /etc/chilli/www/coova.html
+fi
+
 log_message "first_boot: removing first_boot marker file"
 rm /sbin/wifimesh/first_boot
 
