c6701ce2a925eb2b43c1895247bca7d4edecba8d
Cody Cooper <cody@codycooper.co.nz>
2013-02-02 15:02:28 +1300

    Clarifies IP hexing

--- wifimesh/files/sbin/wifimesh/settings.sh
+++ wifimesh/files/sbin/wifimesh/settings.sh
@@ -25,10 +25,10 @@ radio_mesh="radio0"
 mac_lan=$(ifconfig eth0 | grep 'HWaddr' | awk '{ print $5 }')
 mac_wan=$(ifconfig br-wan | grep 'HWaddr' | awk '{ print $5 }')
 mac_wlan=$(cat /sys/class/ieee80211/phy0/macaddress)
-ip_lan="10.$(hex_ip -c13-14).$(hex_ip -c16-17).1"
+ip_lan="10.$(hex_ip -c 13-14).$(hex_ip -c 16-17).1"
 ip_dhcp=$(ifconfig br-wan | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1 }')
 ip_gateway=$(route -n | grep 'UG' | awk '{ print $2 }')
-ssid="wifimesh_$(hex_ip -c16-17)"
+ssid="wifimesh_$(hex_ip -c 16-17)"
 
 # Get the dashboard and upgrade server urls
 dashboard_server=$(cat /sbin/wifimesh/dashboard_server.txt)
