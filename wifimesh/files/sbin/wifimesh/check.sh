7bf6ace5b96319eae449bfb14a35fd04b72e9360
Cody Cooper <cody@codycooper.co.nz>
2013-02-18 19:33:28 +1300

    Orphans nodes will now reconnect to the dashboard

--- wifimesh/files/sbin/wifimesh/check.sh
+++ wifimesh/files/sbin/wifimesh/check.sh
@@ -5,8 +5,71 @@
 # Load in the settings
 . /sbin/wifimesh/settings.sh
 
+change_mesh_channel() {
+	# Change channels to the one sent to this function
+	uci set wireless.radio0.channel="$1"
+	uci commit wireless
+	wifi
+	
+	# Wait a little bit for the mesh to initialise
+	sleep 10
+	
+	if [ -z "$(iw wlan0-4 mpath dump | grep '0x')" == "true" ]; then
+		# We have mesh routes now, we can connect to the dashboard to make sure that this is the correct channel
+		curl -A "WMF/v${fw_ver} (http://www.wifi-mesh.co.nz/)" -k -s "http://${dashboard_server}checkin-wm.php?ip=${ip_lan}&mac_lan=${mac_lan}&mac_wan=${mac_wan}&mac_wlan=${mac_wlan}&mac_mesh=${mac_mesh}&action=channel-config" > /tmp/checkin/orphan_channel
+		curl_result=$?
+		curl_data=$(cat /tmp/checkin/orphan_channel)
+		
+		if [ "$curl_result" -eq "0" ]; then
+			uci set wireless.radio0.channel="${curl_data}"
+			uci commit wireless
+			wifi
+		fi
+		
+		# Say that all is well
+		echo "true"
+	else
+		# Nope, it's not this channel
+		echo "false"
+	fi
+}
+
 echo "WiFi Mesh Connection Checker"
 echo "----------------------------------------------------------------"
+
+# Checks mesh connectivity if the node is a repeater (to make sure it hasn't been orphaned)
+if [ "${role}" == "R" ]; then
+	if [ -z "$(iw wlan0-4 mpath dump | grep '0x')" ]; then
+		crontab -r
+		log_message "check: orphan: we have an orphaned node, checking channels!"
+
+		if [ "$(change_mesh_channel 1)" == "true" ]; then
+			log_message "check: orphan: found neighbours on channel 1"
+		elif [ "$(change_mesh_channel 5)" == "true" ]; then
+			log_message "check: orphan: found neighbours on channel 5"
+		elif [ "$(change_mesh_channel 6)" == "true" ]; then
+			log_message "check: orphan: found neighbours on channel 6"
+		elif [ "$(change_mesh_channel 11)" == "true" ]; then
+			log_message "check: orphan: found neighbours on channel 11"
+		elif [ "$(change_mesh_channel 2)" == "true" ]; then
+			log_message "check: orphan: found neighbours on channel 2"
+		elif [ "$(change_mesh_channel 3)" == "true" ]; then
+			log_message "check: orphan: found neighbours on channel 3"
+		elif [ "$(change_mesh_channel 4)" == "true" ]; then
+			log_message "check: orphan: found neighbours on channel 4"
+		elif [ "$(change_mesh_channel 7)" == "true" ]; then
+			log_message "check: orphan: found neighbours on channel 7"
+		elif [ "$(change_mesh_channel 8)" == "true" ]; then
+			log_message "check: orphan: found neighbours on channel 8"
+		elif [ "$(change_mesh_channel 9)" == "true" ]; then
+			log_message "check: orphan: found neighbours on channel 9"
+		elif [ "$(change_mesh_channel 10)" == "true" ]; then
+			log_message "check: orphan: found neighbours on channel 10"
+		else
+			log_message "check: orphan: WARNING: could not recover orphan node!"
+		fi
+	fi
+fi
 
 # Chucks out any bad mesh paths that may be added from time to time
 iw wlan0-4 mpath dump | grep '00:00:00:00:00:00' | while read line; do
