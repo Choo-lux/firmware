45ff663326b951164cc803c531f765e5ed673dfc
WiFiRUSH Admin <vpizzo@wifirush.com>
2013-02-23 16:43:56 -0500

    Support for modular upgrades of www directory
    This change adds support for upgrades to the www directory child directories in the case where the upgrade contains a www.zip package.

--- wifimesh/files/sbin/wifimesh/upgrade.sh
+++ wifimesh/files/sbin/wifimesh/upgrade.sh
@@ -28,18 +28,38 @@ elif [ "$old_package_version" != "$new_package_version" ]; then
 	
 	# Make sure no old upgrade exists
 	if [ -e "/tmp/scripts.zip" ]; then rm "/tmp/scripts.zip"; fi
+	if [ -e "/tmp/www.zip" ]; then rm "/tmp/www.zip"; fi
 	
 	echo "Downloading package upgrade"
 	curl -A "WMF/v${fw_ver} (http://www.wifi-mesh.co.nz/)" -k -s -o /tmp/scripts.zip "http://${firmware_server}firmware/${firmware_branch}/scripts.zip?r=$(head -30 /dev/urandom | tr -dc "0123456789" | head -c3)" > /dev/null
+	curl -A "WMF/v${fw_ver} (http://www.wifi-mesh.co.nz/)" -k -s -o /tmp/www.zip "http://${firmware_server}firmware/${firmware_branch}/www.zip?r=$(head -30 /dev/urandom | tr -dc "0123456789" | head -c3)" > /dev/null
 	
-	echo "Checking validity"
+###### NEW www upgrade package support - BEGIN
+	echo "Checking validity of the www archive"
+	actual_hash=$(curl -A "WMF/v${fw_ver} (http://www.wifi-mesh.co.nz/)" -s "http://${firmware_server}firmware/${firmware_branch}/whash.txt?r=$(head -30 /dev/urandom | tr -dc "0123456789" | head -c3)")
+	local_hash=$(md5sum /tmp/www.zip | awk '{ print $1 }')
+	
+	if [ ! -e "/tmp/www.zip" ]; then
+		log_message "upgrade: The www package upgrade download was not successful, upgrade cancelled."
+	elif [ "${actual_hash}" = "${local_hash}" ]; then
+		echo "Installing www upgrade"
+		unzip -o /tmp/www.zip -d /www > /dev/null
+		
+		# Say about it
+		log_message "upgrade: Upgraded www package from ${old_package_version} to ${new_package_version} successfully"
+	else
+		log_message "upgrade: The downloaded www package upgrade was not valid, www package upgrade cancelled."
+	fi
+###### NEW www upgrade package support - END	
+
+	echo "Checking validity of the scripts archive"
 	actual_hash=$(curl -A "WMF/v${fw_ver} (http://www.wifi-mesh.co.nz/)" -s "http://${firmware_server}firmware/${firmware_branch}/hash.txt?r=$(head -30 /dev/urandom | tr -dc "0123456789" | head -c3)")
 	local_hash=$(md5sum /tmp/scripts.zip | awk '{ print $1 }')
 	
 	if [ ! -e "/tmp/scripts.zip" ]; then
-		log_message "upgrade: The package upgrade download was not successful, upgrade cancelled."
+		log_message "upgrade: The scripts package upgrade download was not successful, upgrade cancelled."
 	elif [ "${actual_hash}" = "${local_hash}" ]; then
-		echo "Installing upgrade"
+		echo "Installing scripts upgrade"
 		unzip -o /tmp/scripts.zip -d /sbin/wifimesh > /dev/null
 		
 		# Move the startup file
@@ -74,12 +94,12 @@ cat > /etc/banner << banner_end
 banner_end
 		
 		# Say about it
-		log_message "upgrade: Upgraded package from ${old_package_version} to ${new_package_version} successfully, rebooting..."
+		log_message "upgrade: Upgraded the scripts package from ${old_package_version} to ${new_package_version} successfully, rebooting..."
 		
 		reboot
 	else
-		log_message "upgrade: The downloaded package upgrade was not valid, upgrade cancelled."
-	fi
+		log_message "upgrade: The downloaded scripts package upgrade was not valid, upgrade cancelled."
+	fi	
 else
 	log_message "upgrade: v${old_package_version} is the latest package version available."
 fi
