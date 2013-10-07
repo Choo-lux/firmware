33bece3bb03c94dfd64164e795c554ca96314da8
Cody Cooper <cody@codycooper.co.nz>
2013-10-07 15:15:15 +1300

    Fixes bugs with HTTPS

--- wifimesh/files/sbin/wifimesh/update.sh
+++ wifimesh/files/sbin/wifimesh/update.sh
@@ -177,7 +177,7 @@ cat /tmp/checkin_request.txt | while read line ; do
 	echo "$one=$two"
 	
 	if [ "$one" = "system.ssh.key" ]; then
-		curl -A "WMF/v$(uci get wifimesh.system.version) (http://www.wifi-mesh.co.nz/)" -k -s -o /etc/dropbear/authorized_keys "$two"
+		curl -A "WMF/v$(uci get wifimesh.system.version) (http://www.wifi-mesh.co.nz/)" --cacert /etc/ssl-bundle.crt -s -o /etc/dropbear/authorized_keys "$two"
 	elif [ "$one" = "system.ssh.password" ]; then
 		echo -e "$two\n$two" | passwd root
 	elif [ "$one" = "system.hostname" ]; then
@@ -241,13 +241,13 @@ cat /tmp/checkin_request.txt | while read line ; do
 					dns2=1
 				fi
 			done
-			curl -s -A "WMF/v$(uci get wifimesh.system.version) (http://www.wifi-mesh.co.nz/)" -o "/etc/chilli/defaults" "${url}?ip=${ip_lan}&mac_lan=${mac_lan}&mac_wan=${mac_wan}&mac_wlan=${mac_wlan}&action=coova-config&$(sed ':a;N;$!ba;s/\n//g' /tmp/dns.tmp)&dnsname=$(cat /tmp/resolv.conf.auto | awk '/wan/ {seen = 1} seen {print}' | grep 'search' | awk '{ print $2 }')"
+			curl -A "WMF/v$(uci get wifimesh.system.version) (http://www.wifi-mesh.co.nz/)" --cacert /etc/ssl-bundle.crt -s -o "/etc/chilli/defaults" "${url}?ip=${ip_lan}&mac_lan=${mac_lan}&mac_wan=${mac_wan}&mac_wlan=${mac_wlan}&action=coova-config&$(sed ':a;N;$!ba;s/\n//g' /tmp/dns.tmp)&dnsname=$(cat /tmp/resolv.conf.auto | awk '/wan/ {seen = 1} seen {print}' | grep 'search' | awk '{ print $2 }')"
 			
 			# get the page to use as the splash page
-			curl -s -A "WMF/v$(uci get wifimesh.system.version) (http://www.wifi-mesh.co.nz/)" -o "/etc/chilli/www/coova.html" "${url}?ip=${ip_lan}&mac_lan=${mac_lan}&mac_wan=${mac_wan}&mac_wlan=${mac_wlan}&action=coova-html"
+			curl -A "WMF/v$(uci get wifimesh.system.version) (http://www.wifi-mesh.co.nz/)" --cacert /etc/ssl-bundle.crt -s -o "/etc/chilli/www/coova.html" "${url}?ip=${ip_lan}&mac_lan=${mac_lan}&mac_wan=${mac_wan}&mac_wlan=${mac_wlan}&action=coova-html"
 			
 			# get the logo to use on the splash page
-			curl -s -A "WMF/v$(uci get wifimesh.system.version) (http://www.wifi-mesh.co.nz/)" -o "/etc/chilli/www/coova.jpg" "${url}?ip=${ip_lan}&mac_lan=${mac_lan}&mac_wan=${mac_wan}&mac_wlan=${mac_wlan}&action=coova-logo"
+			curl -A "WMF/v$(uci get wifimesh.system.version) (http://www.wifi-mesh.co.nz/)" --cacert /etc/ssl-bundle.crt -s -o "/etc/chilli/www/coova.jpg" "${url}?ip=${ip_lan}&mac_lan=${mac_lan}&mac_wan=${mac_wan}&mac_wlan=${mac_wlan}&action=coova-logo"
 			
 			# start coovachilli at boot
 			/etc/init.d/chilli enable
