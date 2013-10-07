d6fd128bc7bedc0ea4c89df8d40b4a0192f9ec1b
Cody Cooper <cody@codycooper.co.nz>
2013-10-07 15:41:22 +1300

    Fixes bugs with upgrade process

--- wifimesh/files/sbin/wifimesh/upgrade.sh
+++ wifimesh/files/sbin/wifimesh/upgrade.sh
@@ -36,39 +36,46 @@ echo "Latest version number: v$(cat /tmp/upgrade/version)"
 
 echo "Getting latest version hashes and filenames"
 curl -A "WMF/v$(uci get wifimesh.system.version) (http://www.wifi-mesh.co.nz/)" --cacert /etc/ssl-bundle.crt -s -o /tmp/upgrade/md5sums "${url}md5sums?r=$(head -30 /dev/urandom | tr -dc '0123456789' | head -c3)"
+curl_result=$?
 
-if [ "${new_version+x}" = x ] && [ -z "$new_version" ]; then
-	log_message "upgrade: Could not connect to the upgrade server, exiting..."
-elif [ "$(uci get wifimesh.system.version)" != "$new_version" ]; then
-	# Make sure no old firmware exists
-	if [ -e "/tmp/firmware.bin" ]; then rm "/tmp/firmware.bin"; fi
-	
-	echo "Checking for upgrade binary"
-	if grep -q "$(cat /tmp/sysinfo/board_name)-squashfs-sysupgrade" /tmp/md5sums; then
-		echo "Downloading upgrade binary: $(grep $(cat /tmp/sysinfo/board_name)'-squashfs-sysupgrade' /tmp/md5sums | awk '{ print $2 }' | sed 's/*//')"
-		curl -A "WMF/v$(uci get wifimesh.system.version) (http://www.wifi-mesh.co.nz/)" --cacert /etc/ssl-bundle.crt -s -o /tmp/firmware.bin "${url}$(grep $(cat /tmp/sysinfo/board_name)'-squashfs-sysupgrade' /tmp/md5sums | awk '{ print $2 }' | sed 's/*//')?r=$(head -30 /dev/urandom | tr -dc "0123456789" | head -c3)" > /dev/null
-		
-		# Stop if the firmware file does not exist
-		if [ ! -e "/tmp/firmware.bin" ]; then
-			echo "The upgrade binary download was not successful, exiting..."
-		
-		# If the hash is correct: flash the firmware
-		elif [ "$(grep $(cat /tmp/sysinfo/board_name)'-squashfs-sysupgrade' /tmp/md5sums | awk '{ print $1 }')" = "$(md5sum /tmp/firmware.bin | awk '{ print $1 }')" ]; then
-			logger "Installing upgrade binary..."
+if [ "${curl_result}" -eq 0 ]; then
+	if grep -q "." /tmp/upgrade/version; then
+		if [ "$(uci get wifimesh.system.version)" != "$(cat /tmp/upgrade/version)" ]; then
+			# Make sure no old firmware exists
+			if [ -e "/tmp/firmware.bin" ]; then rm "/tmp/firmware.bin"; fi
 			
-			uci set wifimesh.system.version=${new_version}
-			uci set wifimesh.system.first_boot=1
-			uci commit wifimesh
-			
-			sysupgrade -c -d 600 /tmp/firmware.bin
-			
-		# The hash is invalid, stopping here
+			echo "Checking for upgrade binary"
+			if grep -q "$(cat /tmp/sysinfo/board_name)-squashfs-sysupgrade" /tmp/upgrade/md5sums; then
+				echo "Downloading upgrade binary: $(grep $(cat /tmp/sysinfo/board_name)'-squashfs-sysupgrade' /tmp/upgrade/md5sums | awk '{ print $2 }' | sed 's/*//')"
+				curl -A "WMF/v$(uci get wifimesh.system.version) (http://www.wifi-mesh.co.nz/)" --cacert /etc/ssl-bundle.crt -s -o /tmp/firmware.bin "${url}$(grep $(cat /tmp/sysinfo/board_name)'-squashfs-sysupgrade' /tmp/upgrade/md5sums | awk '{ print $2 }' | sed 's/*//')?r=$(head -30 /dev/urandom | tr -dc "0123456789" | head -c3)" > /dev/null
+				
+				# Stop if the firmware file does not exist
+				if [ ! -e "/tmp/firmware.bin" ]; then
+					echo "The upgrade binary download was not successful, exiting..."
+				
+				# If the hash is correct: flash the firmware
+				elif [ "$(grep $(cat /tmp/sysinfo/board_name)'-squashfs-sysupgrade' /tmp/upgrade/md5sums | awk '{ print $1 }')" = "$(md5sum /tmp/firmware.bin | awk '{ print $1 }')" ]; then
+					logger "Installing upgrade binary..."
+					
+					uci set wifimesh.system.version=${new_version}
+					uci set wifimesh.system.first_boot=1
+					uci commit wifimesh
+					
+					sysupgrade -c -d 600 /tmp/firmware.bin
+					
+				# The hash is invalid, stopping here
+				else
+					echo "The upgrade binary hash did not match, exiting..."
+				fi	
+			else
+				echo "There is no upgrade binary for this device ($(cat /tmp/sysinfo/model)/$(cat /tmp/sysinfo/board_name)), exiting..."
+			fi
 		else
-			echo "The upgrade binary hash did not match, exiting..."
-		fi	
+			echo "v$(cat /tmp/upgrade/version) is the latest firmware version available."
+		fi
 	else
-		echo "There is no upgrade binary for this device ($(cat /tmp/sysinfo/model)/$(cat /tmp/sysinfo/board_name)), exiting..."
+		echo "Could not connect to the upgrade server, exiting..."
 	fi
 else
-	echo "v$(cat /tmp/upgrade/version) is the latest firmware version available."
+	echo "Could not connect to the upgrade server, exiting..."
 fi
