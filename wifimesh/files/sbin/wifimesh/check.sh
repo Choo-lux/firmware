40776c0ea0b04e0eb7ffa317ede28126c4184f4e
Cody Cooper <cody@codycooper.co.nz>
2013-10-14 20:54:32 +1300

    Fixes bug with 'check' not being able to be disabled, fixes dns check that didn't work and adds support for LEDs on UBNT Bullets & Open-Mesh OM2Ps

--- wifimesh/files/sbin/wifimesh/check.sh
+++ wifimesh/files/sbin/wifimesh/check.sh
@@ -9,10 +9,13 @@ echo "WiFi Mesh Connection Checker"
 echo "----------------------------------------------------------------"
 
 # Check that we are allowed to use this
-if [ "$(uci get wifimesh.check.enabled)" -eq 0 ]; then
+if [ "$(uci get wifimesh.ping.enabled)" -eq 0 ]; then
 	echo "This script is disabled, exiting..."
 	exit
 fi
+
+# Define the default status
+bad_status="0"
 
 # Deletes any bad mesh paths that may occur from time to time
 iw ${if_mesh} mpath dump | grep '00:00:00:00:00:00' | while read line; do
@@ -23,6 +26,7 @@ done
 if [ "$(ping -c 2 ${ip_gateway})" ]; then
 	lan_status=1
 else
+	bad_status=1
 	lan_status=0
 fi
 
@@ -30,14 +34,52 @@ fi
 if [ "$(ping -c 2 $(uci get wifimesh.ping.server))" ]; then
 	wan_status=1
 else
+	bad_status=1
 	wan_status=0
 fi
 
 # Tests DNS Connectivity
-if [ "$(nslookup $(uci get wifimesh.ping.server))" ]; then
+nslookup $(uci get wifimesh.ping.server) > /dev/null
+dns_temp=$?
+
+if [ "${dns_temp}" -eq 0 ]; then
 	dns_status=1
 else
+	bad_status=1
 	dns_status=0
+fi
+
+# Use the LEDs
+if [ "${bad_status}" -eq 1 ]; then
+	if [ "$(cat /tmp/sysinfo/board_name)" = "bullet-m" ]; then
+		echo 0 > /sys/class/leds/ubnt:green:link4/brightness
+		echo 0 > /sys/class/leds/ubnt:green:link3/brightness
+		echo 0 > /sys/class/leds/ubnt:orange:link2/brightness
+		echo 0 > /sys/class/leds/ubnt:red:link1/brightness
+		echo "timer" > /sys/class/leds/ubnt:red:link1/trigger
+		echo 5000 > /sys/class/leds/ubnt:red:link1/delay_on
+		echo 1000 > /sys/class/leds/ubnt:red:link1/delay_off
+	elif [ "$(cat /tmp/sysinfo/board_name)" = "om2p" ]; then
+		echo 0 > /sys/class/leds/om2p:green:wifi/brightness
+		echo 0 > /sys/class/leds/om2p:yellow:wifi/brightness
+		echo 0 > /sys/class/leds/om2p:red:wifi/brightness
+		echo "timer" > /sys/class/leds/om2p:red:wifi/trigger
+		echo 5000 > /sys/class/leds/om2p:red:wifi/delay_on
+		echo 1000 > /sys/class/leds/om2p:red:wifi/delay_off
+	fi
+else
+	if [ "$(cat /tmp/sysinfo/board_name)" = "bullet-m" ]; then
+		echo "none" > /sys/class/leds/ubnt:red:link1/trigger
+		echo 1 > /sys/class/leds/ubnt:green:link4/brightness
+		echo 1 > /sys/class/leds/ubnt:green:link3/brightness
+		echo 1 > /sys/class/leds/ubnt:orange:link2/brightness
+		echo 1 > /sys/class/leds/ubnt:red:link1/brightness
+	elif [ "$(cat /tmp/sysinfo/board_name)" = "om2p" ]; then
+		echo "none" > /sys/class/leds/om2p:red:wifi/trigger
+		echo 1 > /sys/class/leds/om2p:green:wifi/brightness
+		echo 0 > /sys/class/leds/om2p:yellow:wifi/brightness
+		echo 0 > /sys/class/leds/om2p:red:wifi/brightness
+	fi
 fi
 
 # Log that result
